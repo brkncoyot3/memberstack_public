cac05247fad4e0e8647938f17ec8cbc75f4ba4f2
brkncoyot3 <52013198+brkncoyot3@users.noreply.github.com>
2020-10-13 10:10:41 -0700

    Update redirect_based_on_iscustomer.js

--- redirect_based_on_iscustomer.js
+++ redirect_based_on_iscustomer.js
@@ -1,17 +1,19 @@
 !window.addEventListener('DOMContentLoaded', (e) => {
 	MemberStack.onReady.then(function(member) {
-  	// if member is logged in
+  		// if member is logged in
 		if(member.loggedIn){
-        // grab member "iscustomer" value
+        		
+			// grab member "iscustomer" value
 		        var iscustomer = member["iscustomer"]
-	        // if member "iscustomer" == true, log it
+			
+	        	// if member "iscustomer" == true, log it
 			if(iscustomer == "true")
-                 console.log("User is a customer")
-	        // else "iscustomer" !== true, redirect
+                 		console.log("User is a customer")
+			
+	        	// else "iscustomer" !== true, redirect
             		else {
-                window.location.href = "/not-a-customer";
-                console.log("User is not a customer")
-            }
+                		window.location.href = "/not-a-customer";
+            		}
 		}
 		// user not logged in
 	  	else{
